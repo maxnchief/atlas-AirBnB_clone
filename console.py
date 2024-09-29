40ad2ae10b244b7e82852938d2ac9a159ec337aa
Malik Vance <malikovance@gmail.com>
2024-09-29 10:55:56 -0500

    updated console.py

--- console.py
+++ console.py
@@ -1,29 +1,61 @@
 #!/usr/bin/python3
-"""AirBnB Clone - Command Interpreter"""
-
 import cmd
-import sys
-import models
-from models.base_model import BaseModel
-
+from models import storage
+from models.user import User
 
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for AirBnB objects"""
+    prompt = '(hbnb) '
 
-    prompt = "(hbnb) "
+    def do_create(self, arg):
+        """Creates a new User"""
+        user = User()
+        user.first_name = input("First Name: ")
+        user.last_name = input("Last Name: ")
+        user.email = input("Email: ")
+        user.password = input("Password: ")
+        user.save()
+        print(user.id)
 
-    def do_quit(self, arg):
-        """Quit command to exit the program"""
-        return True
+    def do_show(self, arg):
+        """Shows a User by ID"""
+        args = arg.split()
+        if len(args) < 1:
+            print("** user id missing **")
+            return
+        user = storage.all().get(f"User.{args[0]}")
+        if user:
+            print(user)
+        else:
+            print("** no user found **")
 
-    def do_EOF(self, arg):
-        """Exit the program with EOF (Ctrl+D)"""
-        return True
+    def do_destroy(self, arg):
+        """Destroys a User by ID"""
+        args = arg.split()
+        if len(args) < 1:
+            print("** user id missing **")
+            return
+        user_key = f"User.{args[0]}"
+        if user_key in storage.all():
+            del storage.all()[user_key]
+            storage.save()
+        else:
+            print("** no user found **")
 
-    def emptyline(self):
-        """Do nothing on empty line"""
-        pass
+    def do_update(self, arg):
+        """Updates a User's attributes"""
+        args = arg.split()
+        if len(args) < 4:
+            print("** insufficient arguments **")
+            return
+        user_key = f"User.{args[0]}"
+        user = storage.all().get(user_key)
+        if user:
+            setattr(user, args[1], args[2])
+            user.save()
+        else:
+            print("** no user found **")
 
-
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    def do_all(self, arg):
+        """Shows all Users"""
+        users = [str(user) for user in storage.all().values() if isinstance(user, User)]
+        print(users)
