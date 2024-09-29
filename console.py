a4b8f043411a07ce2274bcefc35508b399c1aaf6
maxnchief <logandogan@gmail.com>
2024-09-29 16:02:57 -0500

    added do help and do EOF

--- console.py
+++ console.py
@@ -6,6 +6,13 @@ from models.user import User
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
+    def do_help(self, arg: str) -> bool | None:
+        return super().do_help(arg)
+    
+    def do_EOF(self,line):
+        print("")
+        return True
+
     def do_create(self, arg):
         """Creates a new User"""
         user = User()
