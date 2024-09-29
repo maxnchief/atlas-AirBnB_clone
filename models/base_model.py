7fc2a20d9b63966bb620d1dec838b9eebbf0f1d0
Malik Vance <malikovance@gmail.com>
2024-09-29 11:06:59 -0500

    fixed issues in base model and user. also added init method to user class.

--- models/base_model.py
+++ models/base_model.py
@@ -5,12 +5,12 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id = str(uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """Updates the `updated_at` attribute and saves the object."""
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
         from models import storage  # Lazy loading to avoid circular import
         storage.save()  # Call the save method on the storage
 
@@ -18,6 +18,9 @@ class BaseModel:
         """Returns a dictionary containing all keys/values of the instance."""
         dictionary = self.__dict__.copy()
         dictionary['__class__'] = self.__class__.__name__
+        # Convert datetime objects to ISO format strings
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
 
     def __str__(self):
--- models/user.py
+++ models/user.py
@@ -2,7 +2,13 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    
+    def __init__(self, email="", password="", first_name="", last_name="", *args, **kwargs):
+        super().__init__(*args, **kwargs)  # Call the BaseModel initializer
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"[User] ({self.id}) {{'email': '{self.email}', 'first_name': '{self.first_name}', 'last_name': '{self.last_name}'}}"
