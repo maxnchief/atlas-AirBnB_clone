74fcc29b919c03efb2f9db4d89071fe9c18551d3
maxnchief <logandogan@gmail.com>
2024-09-29 15:33:18 -0500

    moved public class to before the init

--- models/user.py
+++ models/user.py
@@ -3,12 +3,13 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class that inherits from BaseModel"""
     
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)  # Call the BaseModel initializer
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        super().__init__(*args, **kwargs)   #Call the BaseModel initializer
+
 
     def __str__(self):
         return f"[User] ({self.id}) {{'email': '{self.email}', 'first_name': '{self.first_name}', 'last_name': '{self.last_name}'}}"
