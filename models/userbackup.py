5ddd21541623bcb89081ee5f849d2f17f4ceb6af
maxnchief <logandogan@gmail.com>
2024-09-29 15:25:01 -0500

    turned the private atributes into public atributes

--- models/userbackup.py
+++ models/userbackup.py
@@ -3,12 +3,12 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class that inherits from BaseModel"""
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, email="", password="", first_name="", last_name="", *args, **kwargs):
         super().__init__(*args, **kwargs)  # Call the BaseModel initializer
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
 
     def __str__(self):
         return f"[User] ({self.id}) {{'email': '{self.email}', 'first_name': '{self.first_name}', 'last_name': '{self.last_name}'}}"
