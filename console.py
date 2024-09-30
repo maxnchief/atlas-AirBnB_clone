4132b207fd4b6b94eb33767bba933cc2905ce284
Malik Vance <malikovance@gmail.com>
2024-09-29 20:15:02 -0500

    added create, show, destroy, all, and update. these are new instances.

--- console.py
+++ console.py
@@ -1,86 +1,140 @@
 #!/usr/bin/python3
-'''this is the console module'''
+'''This is the console module.'''
 
 import cmd
 from models import storage
 from models.user import User
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
-    def do_help(self, arg):
-        return super().do_help(arg)
-    
-    def do_EOF(self):
-        print("")
-        return True
-    
-    def do_quit(self, args):
-        '''Quits the program'''
-        return True        
-        
-    def emptyline(self):
-        pass
-
     def do_create(self, arg):
-        """Creates a new User"""
-        try:
-            user = User()
-            user.first_name = input("First Name: ")
-            user.last_name = input("Last Name: ")
-            user.email = input("Email: ")
-            user.password = input("Password: ")
-            user.save()
-            print(user.id)
-        except Exception as e:
-            print(f"Error creating user: {e}")
+        """Creates a new instance of BaseModel, saves it, and prints the id."""
+        if not arg:
+            print("** class name missing **")
+            return
+        if arg not in ["BaseModel", "User", "Amenity", "City", "Place", "Review", "State"]:
+            print("** class doesn't exist **")
+            return
+        
+        # Create instance and save
+        instance = eval(arg)()
+        instance.save()
+        print(instance.id)
 
     def do_show(self, arg):
-        """Shows a User by ID"""
+        """Prints the string representation of an instance based on the class name and id."""
         args = arg.split()
-        if len(args) < 1:
-            print("** user id missing **")
+        if not args:
+            print("** class name missing **")
             return
-        user = storage.all().get(f"User.{args[0]}")
-        if user:
-            print(user)
+        if args[0] not in ["BaseModel", "User", "Amenity", "City", "Place", "Review", "State"]:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_key = f"{args[0]}.{args[1]}"
+        instance = storage.all().get(instance_key)
+        if instance:
+            print(instance)
         else:
-            print("** no user found **")
+            print("** no instance found **")
 
     def do_destroy(self, arg):
-        """Destroys a User by ID"""
+        """Deletes an instance based on the class name and id."""
         args = arg.split()
-        if len(args) < 1:
-            print("** user id missing **")
+        if not args:
+            print("** class name missing **")
             return
-        user_key = f"User.{args[0]}"
-        if user_key in storage.all():
-            del storage.all()[user_key]
+        if args[0] not in ["BaseModel", "User", "Amenity", "City", "Place", "Review", "State"]:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_key = f"{args[0]}.{args[1]}"
+        if instance_key in storage.all():
+            del storage.all()[instance_key]
             storage.save()
         else:
-            print("** no user found **")
-
-    def do_update(self, arg):
-        """Updates a User's attributes"""
-        args = arg.split()
-        if len(args) < 4:
-            print("** insufficient arguments **")
-            return
-        user_key = f"User.{args[0]}"
-        user = storage.all().get(user_key)
-        if user:
-            if hasattr(user, args[1]):
-                setattr(user, args[1], args[2])
-                user.save()
-            else:
-                print("** attribute does not exist **")
-        else:
-            print("** no user found **")
+            print("** no instance found **")
 
     def do_all(self, arg):
-        """Shows all Users"""
-        users = [str(user) for user in storage.all().values() if isinstance(user, User)]
-        print(", ".join(users) if users else "No users found.")
+        """Prints all string representation of all instances based or not on the class name."""
+        if arg and arg not in ["BaseModel", "User", "Amenity", "City", "Place", "Review", "State"]:
+            print("** class doesn't exist **")
+            return
+
+        instances = []
+        for key, instance in storage.all().items():
+            if arg:
+                if key.startswith(arg):
+                    instances.append(str(instance))
+            else:
+                instances.append(str(instance))
+        print(instances)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating attribute."""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in ["BaseModel", "User", "Amenity", "City", "Place", "Review", "State"]:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_key = f"{args[0]}.{args[1]}"
+        instance = storage.all().get(instance_key)
+        if not instance:
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        attribute_name = args[2]
+        value = args[3].strip('"')  # Remove quotes if present
+        
+        # Cast value to the appropriate type
+        if value.isdigit():
+            value = int(value)
+        try:
+            value = float(value)
+        except ValueError:
+            pass  # Keep it as a string if conversion fails
+        
+        setattr(instance, attribute_name, value)
+        instance.save()
+
+    def do_EOF(self):
+        """Exits the program."""
+        print("")
+        return True
+
+    def do_quit(self, arg):
+        """Quits the program."""
+        return True
+
+    def emptyline(self):
+        """Do nothing on an empty line."""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
