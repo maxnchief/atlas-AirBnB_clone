42a32ec0fe31ab6f6aa16d64e50b210c1cac4ae7
Malik Vance <malikovance@gmail.com>
2024-09-29 19:37:35 -0500

    added print statement in reload method and message for file not found.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,30 +9,33 @@ class FileStorage:
         self.__objects = {}  # Initialize an empty dictionary to store objects
 
     def all(self):
-        """Returns a dictionary of all stored objects"""
+        """Returns a dictionary of all stored objects."""
         return self.__objects
 
     def new(self, obj):
-        """Adds a new object to the storage"""
+        """Adds a new object to the storage."""
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """Saves the objects to a JSON file"""
+        """Saves the objects to a JSON file."""
         with open(self.__file_path, 'w') as file:
             json.dump({key: obj.to_dict() for key, obj in self.__objects.items()}, file)
 
     def reload(self):
-        """Loads the objects from the JSON file"""
+        """Loads the objects from the JSON file."""
         try:
             with open(self.__file_path, 'r') as file:
                 obj_dict = json.load(file)
                 for key, value in obj_dict.items():
                     class_name = value["__class__"]
+                    # Debugging print
+                    print(f"Reloading object of class {class_name} with key {key}")
                     if class_name == "User":
                         obj = User(**value)
                     else:
                         obj = BaseModel(**value)
                     self.new(obj)
         except FileNotFoundError:
-            pass
+            print("File not found. No objects to load.")
+            pass  # This is fine; it simply means there was nothing to load
