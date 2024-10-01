80ecbc2022bd8ff769224ab48430890f44c1ca91
maxnchief <logandogan@gmail.com>
2024-10-01 14:40:00 -0500

    added test for objects and file path

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@ import os
 import json
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
 
 class TestFileStorage(unittest.TestCase):
     def setUp(self):
@@ -51,6 +52,24 @@ class TestFileStorage(unittest.TestCase):
         key = f"BaseModel.{obj.id}"
         self.assertIn(key, self.storage.all())
         self.assertEqual(self.storage.all()[key].id, obj.id)
+    
+    def test__file_path(self):
+        """Test that the file path is set correctly"""
+        self.assertEqual(self.storage._FileStorage__file_path, "test_file.json")
+
+    def test_objects(self):
+            """Test that objects are stored correctly in __objects"""
+            obj1 = BaseModel()
+            obj2 = User()
+            self.storage.new(obj1)
+            self.storage.new(obj2)
+            key1 = f"BaseModel.{obj1.id}"
+            key2 = f"User.{obj2.id}"
+            self.assertIn(key1, self.storage.all())
+            self.assertIn(key2, self.storage.all())
+            self.assertEqual(self.storage.all()[key1], obj1)
+            self.assertEqual(self.storage.all()[key2], obj2)
+
 
 if __name__ == "__main__":
     unittest.main()
